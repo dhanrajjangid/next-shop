6fc5a0723d7c89d569ee6befe7e59120db7f4da9
RanVeer JanGid02 <rvjangidnov2002@gmail.com>
2024-01-25 02:17:17 +0530

    b

--- src/components/Charts/LineChart.js
+++ src/components/Charts/LineChart.js
@@ -1,6 +1,9 @@
 "use client"
 
 import ReactApexChart from "react-apexcharts";
+import dynamic from 'next/dynamic'
+
+const ReactApexChart = dynamic(() => import('react-apexcharts'), { ssr: false });
 export default function LineCart({ options, series }) {
   return (
     <ReactApexChart
