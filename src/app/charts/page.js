5e305a5fc2aa77c6ec285198839fa4d7851afbaa
RanVeer JanGid02 <rvjangidnov2002@gmail.com>
2024-01-25 02:22:16 +0530

    baa

--- src/app/charts/page.js
+++ src/app/charts/page.js
@@ -32,10 +32,10 @@ export default function ChartsPage() {
               series={IrregularChartSeries}
             />
           </div> */}
-          {(typeof window !== 'undefined') &&
+
           <div className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
             <LineChart options={LineChartOptions} series={LineChartSeries} />
-          </div>}
+          </div>
         </div>
       </div>
     </section>
--- src/components/Charts/LineChart.js
+++ src/components/Charts/LineChart.js
@@ -5,11 +5,15 @@ import dynamic from 'next/dynamic'
 const ReactApexChart = dynamic(() => import('react-apexcharts'), { ssr: false });
 export default function LineCart({ options, series }) {
   return (
+    <>
+    {typeof window !== 'undefined' && 
     <ReactApexChart
       options={options}
       series={series}
       type="line"
       height={350}
-    />
+    />}
+    </>
+    
   );
 }
