49111c6b123b05a14d26531e9517235faee62567
Dhanraj Jangid <44337910+dhanrajjangid@users.noreply.github.com>
2024-01-25 02:36:49 +0530

    Update LineChartData.js

--- src/data/LineChartData.js
+++ src/data/LineChartData.js
@@ -23,21 +23,21 @@ export const LineChartOptions = {
     },
   },
   xaxis: {
-    categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep"],
+    categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep","Jul", "Aug", "Sep"],
   },
 };
 
 export const LineChartSeries = [
   {
     name: "Desktops",
-    data: [10, 41, 35, 51, 49, 62, 69, 91, 148, 120, 82, 105, 132, 170, 130],
+    data: [10, 41, 35, 51, 49, 62, 69, 91, 148, 120, 82, 105],
   },
   {
     name: "Laptops",
-    data: [20, 111, 95, 61, 19, 92, 49, 51, 50, 80, 112, 145, 101, 178, 120],
+    data: [20, 111, 95, 61, 19, 92, 49, 51, 50, 80, 112, 145],
   },
   {
     name: "Chargers",
-    data: [10, 200, 20, 160, 55, 192, 15, 51, 99, 30, 80, 200, 120, 55, 180],
+    data: [10, 200, 20, 160, 55, 192, 15, 51, 99, 30, 80, 200],
   },
 ];
