dec1a17f1dd90033311db135e28151f4e2ad23ea
Dhanraj Jangid <44337910+dhanrajjangid@users.noreply.github.com>
2024-01-25 02:37:12 +0530

    Update page.js

--- src/app/charts/page.js
+++ src/app/charts/page.js
@@ -3,9 +3,9 @@
 // import ColumnChart from "@/components/Charts/ColumnChart";
 // import IrregularChart from "@/components/Charts/IrregularChart";
 // import PieChart from "@/components/Charts/PieChart";
-// import LineChart from "@/components/Charts/LineChart";
+import LineChart from "@/components/Charts/LineChart";
 // import { PieChartDataOptions, PieChartSeries } from "@/data/PieChartData";
-// import { LineChartOptions, LineChartSeries } from "@/data/LineChartData";
+import { LineChartOptions, LineChartSeries } from "@/data/LineChartData";
 // import { ColumnChartOptions, ColumnChartSeries } from "@/data/ColumnChartData";
 // import {
 //   IrregularChartOptions,
@@ -17,7 +17,7 @@ export default function ChartsPage() {
     <section className="bg-white dark:bg-gray-900">
       <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
         <div className="grid gap-8 lg:grid-cols-2">
-          Charts
+          
           {/* <div className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
             <ColumnChart
               options={ColumnChartOptions}
@@ -35,7 +35,7 @@ export default function ChartsPage() {
           </div> */}
 
           <div className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-            {/* <LineChart options={LineChartOptions} series={LineChartSeries} /> */}
+            <LineChart options={LineChartOptions} series={LineChartSeries} />
           </div>
         </div>
       </div>
